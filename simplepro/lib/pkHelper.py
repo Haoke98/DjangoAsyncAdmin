d1a6c242cd304e273e4bf0db891d0d7882566b4a
Sadam·Sadik <1903249375@qq.com>
2023-12-27 12:20:57 +0800

    1.把基类ORM模型的PK改成了UUID格式 && 同时增加了字段remark来可存储小备注/标签.
    Signed-off-by: Sadam·Sadik <1903249375@qq.com>

---
+++ simplepro/lib/pkHelper.py
@@ -0,0 +1,13 @@
+# _*_ codign:utf8 _*_
+"""====================================
+@Author:Sadam·Sadik
+@Email：1903249375@qq.com
+@Date：2023/11/29
+@Software: PyCharm
+@disc:
+======================================="""
+import uuid
+
+
+def uuid_generator():
+    return str(uuid.uuid4())
--- simplepro/models.py
+++ simplepro/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 from simplepro.editor import fields
+from simplepro.lib import pkHelper
 
 
 class BaseModel(models.Model):
-    # id = models.IntegerField(primary_key=True, null=False, auto_created=True, editable=False)
+    id = models.CharField(max_length=48, primary_key=True, editable=False, default=pkHelper.uuid_generator)
     createdAt = models.DateTimeField(auto_created=True, auto_now_add=True, verbose_name="创建时间", null=True,
                                      editable=False, db_index=True)
     updatedAt = models.DateTimeField(auto_now=True, verbose_name="最近更新时间", null=True, editable=False, db_index=True)
     deletedAt = models.DateTimeField(verbose_name="被删除时间", null=True, editable=False, db_index=True)
+    remark = models.CharField(verbose_name="小备注/标签", max_length=100, null=True, blank=True, db_index=True)
     info = fields.UETextField(verbose_name='说明', null=True, blank=True)
 
     class Meta:
